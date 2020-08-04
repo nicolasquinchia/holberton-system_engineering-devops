4f3e8ef6d47a23966d65d79208f78947d6e50d96
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-03 19:01:08 -0500

    0x0A, Configuration Management, create a file, quoted owner and gruop

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file with certain requirements
 file { '/tmp/holberton':
-  path    => 'file',
+  path    => '/tmp/holberton',
   mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
