1599acba3a91a9956c37c5d2e580d2d2ccc541ff
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-04 23:36:56 -0500

    0x0B, puppet, no authentication password

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Set up your client SSH configuration file so that you can connect to a server without typing a password.
+file_line { 'check file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'no password authentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
