83bf3da36c8f93e53fd2872e1d9787e65cb9f285
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-01 00:20:41 -0500

    0x0C config nginx with puppet, delete permissions, check statement for redirections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,23 @@
-# Configuring your server with Puppet!
+# Install Nginx web server with puppet
 package { 'nginx':
-    ensure   => 'lastest',
-    name     => 'nginx',
-    provider => 'apt'
+  ensure   => 'latest',
+  name     => 'nginx',
+  provider => 'apt'
 }
 
-file { 'html':
-    path    => '/var/www/html/index.html',
-    mode    => '0664',
-    content => 'Holberton School'
+file { 'index':
+  path    => '/var/www/html/index.html',
+  mode    => '0664',
+  content => 'Holberton School'
 }
 
-file_line { '301 Moved_perm':
+file_line { '301 Moved Permanently':
   path  => '/etc/nginx/sites-available/default',
-  line  => '\tlocation /redirect_me {\n\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; \n\t}',
+  line  => '\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}',
   after => '^server {'
 }
 
-service { 's_nginx':
+service { 'nginx':
   ensure => running,
   enable => true
 }
