cf2c48fc651f6d028598f0845a498bf81e8652b2
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-31 20:42:49 -0500

    0x0C config nginx with puppet, add semi

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,19 @@
 # Configuring your server with Puppet!
-
 package {'nginx':
-    ensure   => 'installed',
+    ensure   => 'lastest',
     name     => 'nginx',
-    provider => apt,
+    provider => 'apt'
 }
 
 file {'html':
-    path    => '/var/www/html/index.nginx-debian.html'
-    mode    => '0644'
+    path    => '/var/www/html/index.nginx-debian.html',
+    mode    => '0644',
     owner   => 'ubuntu',
     group   => 'ubuntu',
     content => 'Holberton School'
 }
 
-file_line { '301 Moved Permanently':
+file_line { '301 Moved_perm':
   path  => '/etc/nginx/sites-available/default',
   line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   after => '^server {'
