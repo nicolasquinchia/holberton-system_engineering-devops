dead252eb46d9bbb3a9df95a4d0f2ee3c275ae9c
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-31 21:03:42 -0500

    0x0C config nginx with puppet, delete permissions, check statement for redirections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,18 +6,18 @@ package { 'nginx':
 }
 
 file { 'html':
-    path    => '/var/www/html/index.nginx-debian.html',
+    path    => '/var/www/html/index.html',
     mode    => '0664',
     content => 'Holberton School'
 }
 
 file_line { '301 Moved_perm':
   path  => '/etc/nginx/sites-available/default',
-  line  => '\tlocation /redirect_me {\n\t return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; \n\t',
+  line  => '\tlocation /redirect_me {\n\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; \n\t}',
   after => '^server {'
 }
 
-service { 'nginx':
+service { 's_nginx':
   ensure => running,
   enable => true
 }
