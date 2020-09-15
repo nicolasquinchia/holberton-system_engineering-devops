20c60abf3791d74003189e524f1a1973fe1186f4
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-14 23:20:48 -0500

    0x15, Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that returns information
+    about his/her todo list progress.
+    """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/" + argv[1]).json()
+    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    cp_tasks = []
+    for tasks in todos:
+        if tasks["completed"] is True:
+            cp_tasks.append(tasks["title"])
+    print("Employee {} is done with tasks({}}/{}}):".format(
+        user["name"], len(cp_tasks), len(todos)))
+    for task in cp_tasks:
+        print("\t {}".format(task))
