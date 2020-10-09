33e403f2cd0770ef620a4c35b59b4610f9b8d057
nicolasquinchia <nicolasquinchia@gmail.com>
2020-10-08 23:00:21 -0500

    0x1B update requests limit value, check path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 # Fix requests limit on nginx
-exec { 'update ULIMIT value':
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec { 'update_ULIMIT_value':
+  path    => '/usr/local/bin:/bin/',
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
 }
 
-exec { 'nginx restart':
+exec { 'nginx_restart':
   path    => '/usr/bin/',
   command => 'service nginx restart',
 }
